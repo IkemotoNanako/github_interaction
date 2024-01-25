0f8c942b2a4f56b9e87c7eb99b72c71013c2db0a
IkemotoNanako <chibencaizi@gmail.com>
2024-01-26 00:32:21 +0900

    feat: viewの実装

--- lib/ui/search/view/search_page.dart
+++ lib/ui/search/view/search_page.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:github_interaction/core/account.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_interaction/ui/search/view_model/search_page_notifier.dart';
 import 'package:go_router/go_router.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends ConsumerWidget {
   const SearchPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final controller = TextEditingController();
-    final List<Account> accountInfo = [
-      Account(
-        name: 'Flutter',
-        id: 'flutter',
-        iconUrl: 'https://avatars.githubusercontent.com/u/14101776?v=4',
-      )
-    ];
+    final state = ref.watch(searchNotifierProvider);
+    final notifier = ref.watch(searchNotifierProvider.notifier);
+    final accounts = state.accounts;
 
     return Scaffold(
       body: Center(
@@ -23,33 +20,37 @@ class SearchPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
             child: TextFormField(
-              controller: controller,
               decoration: const InputDecoration(
                 icon: Icon(Icons.search),
-                hintText: 'アカウント名を入力してください',
+                hintText: 'input account name',
               ),
               onChanged: (value) {
                 print(value);
+                notifier.searchAccount(value);
               },
             ),
           ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: accountInfo.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(accountInfo[index].iconUrl),
-                ),
-                title: Text(accountInfo[index].name),
-                subtitle: Text(accountInfo[index].id),
-                onTap: () {
-                  print(accountInfo[index].id);
-                  context.go('/search/details');
-                },
-              );
-            },
-          )
+          accounts.isEmpty
+              ? const Center(
+                  child: Text('no result'),
+                )
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: accounts.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(accounts[index].iconUrl),
+                      ),
+                      title: Text(accounts[index].name),
+                      subtitle: Text(accounts[index].id),
+                      onTap: () {
+                        print(accounts[index].id);
+                        context.go('/search/details');
+                      },
+                    );
+                  },
+                )
         ],
       )),
     );
--- lib/ui/search/view_model/search_page_notifier.dart
+++ lib/ui/search/view_model/search_page_notifier.dart
@@ -2,16 +2,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_interaction/core/account.dart';
 import 'package:github_interaction/model/search/search_state.dart';
 
-final mainNotifierProvider = StateNotifierProvider<MainNotifer, SearchState>(
-  (ref) => MainNotifer(),
+final searchNotifierProvider =
+    StateNotifierProvider<SearchNotifier, SearchState>(
+  (ref) => SearchNotifier(),
 );
 
-class MainNotifer extends StateNotifier<SearchState> {
-  MainNotifer() : super(const SearchState());
+class SearchNotifier extends StateNotifier<SearchState> {
+  SearchNotifier() : super(const SearchState());
 
-  void addAccount(Account account) {
-    state = state.copyWith(
-      accounts: [...state.accounts, account],
-    );
+  void searchAccount(String query) async {
+    // 実際はここでAPIを叩くReposittoryを呼び出す
+    final accounts = [
+      const Account(
+        name: 'name',
+        iconUrl: 'https://avatars.githubusercontent.com/u/1?v=4',
+        id: 'id',
+      )
+    ];
+    state = state.copyWith(accounts: accounts);
   }
 }
