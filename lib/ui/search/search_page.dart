7882f8c7e2e84ffd98815dc00c61cb05eac2dde5
IkemotoNanako <chibencaizi@gmail.com>
2024-01-25 23:13:16 +0900

    feat: UIの大枠実装

--- lib/ui/search/search_page.dart
+++ lib/ui/search/search_page.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+class AccountInfo {
+  final String name;
+  final String id;
+  final String iconUrl;
+
+  const AccountInfo({
+    required this.name,
+    required this.id,
+    required this.iconUrl,
+  });
+}
+
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = TextEditingController();
+    final List<AccountInfo> accountInfo = [
+      AccountInfo(
+        name: 'Flutter',
+        id: 'flutter',
+        iconUrl: 'https://avatars.githubusercontent.com/u/14101776?v=4',
+      )
+    ];
+
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: const Text("Details"),
-          onPressed: () {
-            context.go('/search/details');
-          },
-        ),
-      ),
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+            child: TextFormField(
+              controller: controller,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.search),
+                hintText: 'アカウント名を入力してください',
+              ),
+              onChanged: (value) {
+                print(value);
+              },
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: accountInfo.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(accountInfo[index].iconUrl),
+                ),
+                title: Text(accountInfo[index].name),
+                subtitle: Text(accountInfo[index].id),
+                onTap: () {
+                  print(accountInfo[index].id);
+                  context.go('/search/details');
+                },
+              );
+            },
+          )
+        ],
+      )),
     );
   }
 }
